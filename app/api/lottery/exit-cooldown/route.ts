99b933896b3185e07dd0f4fad1b23271d9ed6a37
sukanto01899 <sukanto01899@gmail.com>
2025-10-26 14:55:24 +0600

    chainlink keeper added

--- app/api/lottery/exit-cooldown/route.ts
+++ app/api/lottery/exit-cooldown/route.ts
@@ -1,15 +1,15 @@
-import { exitCooldown } from "@/lib/lotteryControl";
-import { NextResponse } from "next/server";
+// import { exitCooldown } from "@/lib/lotteryControl";
+// import { NextResponse } from "next/server";
 
-export async function GET() {
-  try {
-    const hash = await exitCooldown();
-    if (!hash) {
-      return NextResponse.json({ message: "TX failed" }, { status: 400 });
-    }
+// export async function GET() {
+//   try {
+//     const hash = await exitCooldown();
+//     if (!hash) {
+//       return NextResponse.json({ message: "TX failed" }, { status: 400 });
+//     }
 
-    return NextResponse.json({ message: "TX success" }, { status: 2001 });
-  } catch (error) {
-    return NextResponse.json({ error: "Something wrong" }, { status: 500 });
-  }
-}
+//     return NextResponse.json({ message: "TX success" }, { status: 2001 });
+//   } catch (error) {
+//     return NextResponse.json({ error: "Something wrong" }, { status: 500 });
+//   }
+// }
